9f4dfcdd8f4651b7bc984dff9f8ea7c45129b496
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-10-04 13:11:10 +0200

    Delete limiter.js and helloRoute.js after merging functionality into app.js

--- src/routes/helloRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/hello', (req, res) => {
-  res.json({ message: "Hello world" });
-});
-
-export default router; // Utilisez export default
